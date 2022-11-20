7417f50bb05347218b2417934a2732462e0b19f7
Biruk Gelelcha <byafet3@gmail.com>
2022-11-20 18:10:39 +0000

    creating endpoint that counts each obj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,28 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+stat_dict = {
+    "amenities": "Amenities",
+    "cities": "Cities",
+    "places": "Places",
+    "reviews": "Reviews",
+    "states": "States",
+    "users": "Users"
+}
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """return the status of the api"""
     status = {"status": "OK"}
     return jsonify(status)
 
-
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """create an endpoint that retrieves the
+    number of each objects by type
+    """
+    stat = {}
+    for key, Value in stat_dict.items():
+        stat[key] = storage.count(Value)
+    return jsonify(stat)
 
